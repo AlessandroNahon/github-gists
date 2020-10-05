7925b1adaa359901c791095bd216a2d59be9d7b2
Alessandro Nahon <alessandronahon@gmail.com>
2020-10-05 15:20:24 -0400

    adds user avatar

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import { Typography, Grid, Container } from '@material-ui/core'
+import { Typography, Grid, Container, Avatar, Box } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 import Card from 'components/Card'
 
 const useStyles = makeStyles({
   h1: {
-    paddingTop: 'calc(88px + 2rem)',
+    paddingLeft: '0.5rem',
   },
   grid: {
     flexGrow: 1,
+  },
+  header: {
+    alignItems: 'baseline',
+    paddingTop: 'calc(88px + 2rem)',
+  },
+  avatar: {
+    position: 'relative',
+    top: '0.5rem',
+    height: '3.75rem',
+    width: '3.75rem',
   },
 })
 
@@ -26,9 +36,16 @@ function Result({ gists, data, error, isLoading, isFetching }) {
     <>
       {isFetching ? <span>Refreshing...</span> : null}
       <Container>
-        <Typography gutterBottom variant="h2" className={classes.h1}>
-          {data[0].owner.login}
-        </Typography>
+        <Box display="flex" className={classes.header}>
+          <Avatar
+            alt="Github user"
+            src={data?.[0].owner['avatar_url']}
+            className={classes.avatar}
+          />{' '}
+          <Typography gutterBottom variant="h3" className={classes.h1}>
+            {data?.[0].owner.login}
+          </Typography>
+        </Box>
         <Grid container className={classes.grid} justify="start" spacing={2}>
           {gists.map(f => (
             <Card data={f} />
